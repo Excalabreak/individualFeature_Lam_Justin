4834fb7047b692eff112562d0e9b474d84bd031e
Justin Lam <jlam123321@gmail.com>
2023-05-07 14:11:15 -0700

    added more to input and added input history

--- individualFeature/Assets/Scripts/GameManagerScript.cs
+++ individualFeature/Assets/Scripts/GameManagerScript.cs
@@ -4,8 +4,50 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
+    private static GameManagerScript instance;
+
+    [SerializeField] private GameObject PlayerGameObject;
+    [SerializeField] private GameObject EnemyGameObject;
+    private bool isPlayerLeft = true;
+
     private void Awake()
     {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
         Application.targetFrameRate = 60;
+    }
+
+    private void Update()
+    {
+        CheckPlayerLeft();
+        //Debug.Log(isPlayerLeft);
+    }
+
+    private void CheckPlayerLeft()
+    {
+        if (PlayerGameObject.transform.position.x < EnemyGameObject.transform.position.x)
+        {
+            isPlayerLeft = true;
+        }
+        else
+        {
+            isPlayerLeft = false;
+        }
+    }
+
+    public static GameManagerScript Instance
+    {
+        get { return instance; }
+    }
+
+    public bool IsPlayerLeft
+    {
+        get { return isPlayerLeft; }
     }
 }
--- individualFeature/Assets/Scripts/PlayerInputScript.cs
+++ individualFeature/Assets/Scripts/PlayerInputScript.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerInputScript : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float jump = 20f;
+
+    private bool noInput = true;
 
     private PlayerInputer pi;
     private Rigidbody rb;
 
-    private List<InputData> inputs;
+    //newest input is last index
+    private List<MoveEnum> inputHistory = new List<MoveEnum>();
 
     private void Awake()
     {
@@ -25,24 +29,83 @@ public class PlayerInputScript : MonoBehaviour
 
     private void GetInputs()
     {
-        
+        noInput = true;
         if (pi.Player.Crouch.ReadValue<float>() == 1)
         {
             speed = 0f;
+            AddToInputHistory(MoveEnum.down);
+            noInput = false;
         }
         else
         {
             speed = 5f;
         }
 
-        //add jump later
+        //very jank jump fix later
+        if (pi.Player.Jump.ReadValue<float>() == 1)
+        {
+            rb.AddForce(Vector3.up * jump);
+            AddToInputHistory(MoveEnum.up);
+            noInput = false;
+        }
+
         if (pi.Player.Left.ReadValue<float>() == 1)
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
+            AddToInputHistory(HorDirection("left"));
+            noInput = false;
         }
         if (pi.Player.Right.ReadValue<float>() == 1)
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
+            AddToInputHistory(HorDirection("right"));
+            noInput = false;
+        }
+
+        if (noInput)
+        {
+            AddToInputHistory(MoveEnum.noMove);
+        }
+    }
+
+    private MoveEnum HorDirection(string direction)
+    {
+        if (direction == "left")
+        {
+            if (GameManagerScript.Instance.IsPlayerLeft)
+            {
+                return MoveEnum.back;
+            }
+            else
+            {
+                return MoveEnum.forward;
+            }
+        }
+        else if (direction == "right")
+        {
+            if (!GameManagerScript.Instance.IsPlayerLeft)
+            {
+                return MoveEnum.back;
+            }
+            else
+            {
+                return MoveEnum.forward;
+            }
+        }
+        return MoveEnum.noMove;
+    }
+
+    private void AddToInputHistory(MoveEnum me)
+    {
+        if (inputHistory.Count == 0 || inputHistory[inputHistory.Count-1] != me)
+        {
+            inputHistory.Add(me);
+        }
+
+
+        if(inputHistory.Count == 10)
+        {
+            inputHistory.RemoveAt(0);
         }
     }
 }
